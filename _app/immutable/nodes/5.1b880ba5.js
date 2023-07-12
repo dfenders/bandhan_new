ada123961107cff11384c7fb3d923a6dd5131e7b
unknown <adon.moskal@gmail.com>
2023-07-13 11:54:28 +1200

    Updates

---
+++ _app/immutable/nodes/5.1b880ba5.js
@@ -0,0 +1,16 @@
+import{s,n as t}from"../chunks/scheduler.7c4769d9.js";import{S as n,i as l,g as d,h as c,x as h,k as u,a as m,f as b}from"../chunks/index.40695441.js";function y(i){let e,r=`<div style="max-width: 80ch;font-size: 18px;line-height: 1.5;font-style: italic;text-transform:lowercase;text-align:left;"><b>Taking care of your delicate macrame doll</b> <br/><br/>
+      dear customer, we want to ensure your treasured macrame doll remains as beautiful and enchanting as the day you brought it home.
+      caring for these delicate creations is a breeze, and we&#39;re here to share a few simple tips to help you keep them in pristine condition.
+      <br/><br/> <b>1. Handle with care:</b> macrame dolls are delicate and require gentle handling. avoid tugging or pulling on the knots and fibers, as this may cause damage.
+      treat them with love and care, just as you would with any precious keepsake.
+      <br/><br/> <b>2. brushing out the beauty:</b> give your macrame doll some extra love by brushing it gently with a soft-bristled brush. 
+      this helps maintain its shape and removes any loose fibers or dust that may accumulate over time. a light brushing once in a while will keep your doll looking fresh and vibrant.
+      <br/><br/> <b>3. keep away from moisture:</b> while macrame dolls are resilient, it&#39;s best to keep them away from excessive moisture to avoid any potential damage.
+      avoid placing them in damp or humid areas, and ensure they are stored in a dry, well-ventilated space.
+      <br/><br/> <b>4. display with care:</b> when showcasing your macrame doll, choose a spot away from direct sunlight, as prolonged exposure may cause fading or discoloration.
+      Additionally, avoid displaying them near sources of heat or open flames to prevent any potential hazards.
+      <br/><br/>
+      by following these simple guidelines, you can ensure your macrame doll remains a captivating piece of art, radiating its charm and beauty.
+      embrace the delicate nature of these creations and let them continue to bring magic into your life.
+      <br/><br/>
+      if you have any specific concerns or questions about caring for your macrame doll, feel free to reach out to us. we&#39;re here to assist you and ensure your beloved doll remains a cherished part of your collection.</div>`;return{c(){e=d("div"),e.innerHTML=r,this.h()},l(a){e=c(a,"DIV",{class:!0,"data-svelte-h":!0}),h(e)!=="svelte-bkvw95"&&(e.innerHTML=r),this.h()},h(){u(e,"class","new_section")},m(a,o){m(a,e,o)},p:t,i:t,o:t,d(a){a&&b(e)}}}class f extends n{constructor(e){super(),l(this,e,null,y,s,{})}}export{f as component};
