d5d8906b1193aec10e8ed2c2d5f1b10d994d1815
unknown <adon.moskal@gmail.com>
2023-07-13 10:59:31 +1200

    Updates

--- _app/immutable/chunks/paths.62308ee7.js
+++
@@ -1,1 +0,0 @@
-var s;const a=((s=globalThis.__sveltekit_1etoggn)==null?void 0:s.base)??"/bandhan_new";var e;const t=((e=globalThis.__sveltekit_1etoggn)==null?void 0:e.assets)??a;export{t as a,a as b};
