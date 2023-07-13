7acf017ec1e7b578fbee9fc4454402f3854d1695
unknown <adon.moskal@gmail.com>
2023-07-13 14:35:56 +1200

    Updates

---
+++ _app/immutable/nodes/6.d1cbe596.js
@@ -0,0 +1,54 @@
+import{s as m,n}from"../chunks/scheduler.7c4769d9.js";import{S as w,i as g,g as l,s as b,h as u,x as p,c as v,k as y,l as d,a,f as s}from"../chunks/index.40695441.js";function _(f){let t,c="privacy policy",i,o,h=`This Privacy Policy describes how bandhan.co.nz (the “Site” or “we”) collects, uses, and discloses your Personal Information when you visit or make a purchase from the Site.
+
+    <h2>Contact</h2>
+    After reviewing this policy, if you have additional questions, want more information about our privacy practices, or would like to make a complaint, please contact us by e-mail at <a href="mailto:info@bandhan.co.nz">info@bandhan.co.nz</a> or by mail using the details provided below:
+
+    Bandhan Textiles Ltd., 37 Maitland Street, Central Dunedin, OTA, Dunedin 9016, New Zealand
+
+    <h2>Collecting Personal Information</h2>
+    When you visit the Site, we collect certain information about your device, your interaction with the Site, and information necessary to process your purchases. We may also collect additional information if you contact us for customer support. In this Privacy Policy, we refer to any information about an identifiable individual (including the information below) as “Personal Information”. See the list below for more information about what Personal Information we collect and why.
+
+    <h2>Device information</h2> <b>Purpose of collection:</b> to load the Site accurately for you, and to perform analytics on Site usage to optimize our Site.
+    <b>Source of collection:</b> Collected automatically when you access our Site using cookies.
+    <b>Disclosure for a business purpose:</b> shared with our processor Shopify.
+    <b>Personal Information collected:</b> version of web browser, IP address, time zone, cookie information, what sites or products you view, and how you interact with the Site.
+    <h2>Order information</h2> <b>Purpose of collection:</b> to provide products or services to you to fulfill our contract, to process your payment information, arrange for shipping, and provide you with invoices and/or order confirmations, communicate with you, screen our orders for potential risk or fraud, and when in line with the preferences you have shared with us, provide you with information or advertising relating to our products or services.
+    <b>Source of collection:</b> collected from you.
+    <b>Disclosure for a business purpose:</b> shared with our processor Shopify.
+    <b>Personal Information collected:</b> name, billing address, shipping address, payment information (including credit card numbers), email address, and phone number.
+
+    <h2>Sharing Personal Information</h2>
+    We share your Personal Information with service providers to help us provide our services and fulfill our contracts with you, as described above. For example:
+
+    We use Shopify to power our online store. You can read more about how Shopify uses your Personal Information here: <a href="https://www.shopify.com/legal/privacy">https://www.shopify.com/legal/privacy</a>.
+    We may share your Personal Information to comply with applicable laws and regulations, to respond to a subpoena, search warrant or other lawful request for information we receive, or to otherwise protect our rights.
+
+    <h2>Using Personal Information</h2>
+    We use your personal Information to provide our services to you, which includes: offering products for sale, processing payments, shipping and fulfillment of your order, and keeping you up to date on new products, services, and offers.
+
+    <h2>Cookies</h2>
+    A cookie is a small amount of information that’s downloaded to your computer or device when you visit our Site. We use a number of different cookies, including functional, performance, advertising, and social media or content cookies. Cookies make your browsing experience better by allowing the website to remember your actions and preferences (such as login and region selection). This means you don’t have to re-enter this information each time you return to the site or browse from one page to another. Cookies also provide information on how people use the website, for instance whether it’s their first time visiting or if they are a frequent visitor.
+
+    We use the following cookies to optimize your experience on our Site and to provide our services.
+
+    <h2>Cookies Necessary for the Functioning of the Store</h2> <table><thead><tr><td>Name</td><td>Function</td><td>Duration</td></tr></thead> <tbody><tr><td>_ab	</td><td>Used in connection with access to admin.	</td><td>2y</td></tr> <tr><td>_secure_session_id	</td><td>Used in connection with navigation through a storefront.	</td><td>24h</td></tr> <tr><td>_shopify_country	</td><td>Used in connection with checkout.	</td><td>session</td></tr> <tr><td>_shopify_m	</td><td>Used for managing customer privacy settings.	</td><td>1y</td></tr> <tr><td>_shopify_tm	</td><td>Used for managing customer privacy settings.	</td><td>30min</td></tr> <tr><td>_shopify_tw	</td><td>Used for managing customer privacy settings.	</td><td>2w</td></tr> <tr><td>_storefront_u	</td><td>Used to facilitate updating customer account information.	</td><td>1min</td></tr> <tr><td>_tracking_consent	</td><td>Tracking preferences.	</td><td>1y</td></tr> <tr><td>c	</td><td>Used in connection with checkout.	</td><td>1y</td></tr> <tr><td>cart	</td><td>Used in connection with shopping cart.	</td><td>2w</td></tr> <tr><td>cart_currency	</td><td>Used in connection with shopping cart.	</td><td>2w</td></tr> <tr><td>cart_sig	</td><td>Used in connection with checkout.	</td><td>2w</td></tr> <tr><td>cart_ts	</td><td>Used in connection with checkout.	</td><td>2w</td></tr> <tr><td>cart_ver	</td><td>Used in connection with shopping cart.	</td><td>2w</td></tr> <tr><td>checkout	</td><td>Used in connection with checkout.	</td><td>4w</td></tr> <tr><td>checkout_token	</td><td>Used in connection with checkout.	</td><td>1y</td></tr> <tr><td>dynamic_checkout_shown_on_cart	</td><td>Used in connection with checkout.	</td><td>30min</td></tr> <tr><td>hide_shopify_pay_for_checkout	</td><td>Used in connection with checkout.	</td><td>session</td></tr> <tr><td>keep_alive	</td><td>Used in connection with buyer localization.	</td><td>2w</td></tr> <tr><td>master_device_id	</td><td>Used in connection with merchant login.	</td><td>2y</td></tr> <tr><td>previous_step	</td><td>Used in connection with checkout.	</td><td>1y</td></tr> <tr><td>remember_me	</td><td>Used in connection with checkout.	</td><td>1y</td></tr> <tr><td>secure_customer_sig	</td><td>Used in connection with customer login.	</td><td>20y</td></tr> <tr><td>shopify_pay	</td><td>Used in connection with checkout.	</td><td>1y</td></tr> <tr><td>shopify_pay_redirect	</td><td>Used in connection with checkout.	</td><td>30 minutes, 3w or 1y depending on value</td></tr> <tr><td>storefront_digest	</td><td>Used in connection with customer login.	</td><td>2y</td></tr> <tr><td>tracked_start_checkout	</td><td>Used in connection with checkout.	</td><td>1y</td></tr> <tr><td>checkout_one_experiment	</td><td>Used in connection with checkout.	</td><td>session</td></tr></tbody></table> <h2>Reporting and Analytics</h2> <table><thead><tr><td>Name</td><td>Function</td><td>Duration</td></tr></thead> <tbody><tr><td>_landing_page	</td><td>Track landing pages.	</td><td>2w</td></tr> <tr><td>_orig_referrer	</td><td>Track landing pages.	</td><td>2w</td></tr> <tr><td>_s	</td><td>Shopify analytics.	</td><td>30min</td></tr> <tr><td>_shopify_d	</td><td>Shopify analytics.	</td><td>session</td></tr> <tr><td>_shopify_s	</td><td>Shopify analytics.	</td><td>30min</td></tr> <tr><td>_shopify_sa_p	</td><td>Shopify analytics relating to marketing &amp; referrals.	</td><td>30min</td></tr> <tr><td>_shopify_sa_t	</td><td>Shopify analytics relating to marketing &amp; referrals.	</td><td>30min</td></tr> <tr><td>_shopify_y	</td><td>Shopify analytics.	</td><td>1y</td></tr> <tr><td>_y	</td><td>Shopify analytics.	</td><td>1y</td></tr> <tr><td>_shopify_evids	</td><td>Shopify analytics.	</td><td>session</td></tr> <tr><td>_shopify_ga	</td><td>Shopify and Google Analytics.	</td><td>session</td></tr></tbody></table> <br/>
+
+    The length of time that a cookie remains on your computer or mobile device depends on whether it is a “persistent” or “session” cookie. Session cookies last until you stop browsing and persistent cookies last until they expire or are deleted. Most of the cookies we use are persistent and will expire between 30 minutes and two years from the date they are downloaded to your device.
+
+    You can control and manage cookies in various ways. Please keep in mind that removing or blocking cookies can negatively impact your user experience and parts of our website may no longer be fully accessible.
+
+    Most browsers automatically accept cookies, but you can choose whether or not to accept cookies through your browser controls, often found in your browser’s “Tools” or “Preferences” menu. For more information on how to modify your browser settings or how to block, manage or filter cookies can be found in your browser’s help file or through such sites as: www.allaboutcookies.org.
+
+    Additionally, please note that blocking cookies may not completely prevent how we share information with third parties such as our advertising partners. 
+
+    <h2>Do Not Track</h2>
+    Please note that because there is no consistent industry understanding of how to respond to “Do Not Track” signals, we do not alter our data collection and usage practices when we detect such a signal from your browser.
+
+    <h2>Changes</h2>
+    We may update this Privacy Policy from time to time in order to reflect, for example, changes to our practices or for other operational, legal, or regulatory reasons.
+
+    <h2>Complaints</h2>
+    As noted above, if you would like to make a complaint, please contact us by e-mail or by mail using the details provided under “Contact” above.
+
+    If you are not satisfied with our response to your complaint, you have the right to lodge your complaint with the relevant data protection authority. You can contact your local data protection authority.
+    <br/><br/> <i>Last updated: 7/7/2022</i>`;return{c(){t=l("div"),t.textContent=c,i=b(),o=l("div"),o.innerHTML=h,this.h()},l(e){t=u(e,"DIV",{id:!0,class:!0,style:!0,"data-svelte-h":!0}),p(t)!=="svelte-1f0twbt"&&(t.textContent=c),i=v(e),o=u(e,"DIV",{style:!0,"data-svelte-h":!0}),p(o)!=="svelte-zbtx5a"&&(o.innerHTML=h),this.h()},h(){y(t,"id","wraps"),y(t,"class","divider"),d(t,"margin-top","0"),d(t,"padding-top","0"),d(o,"padding","0 60px 30px")},m(e,r){a(e,t,r),a(e,i,r),a(e,o,r)},p:n,i:n,o:n,d(e){e&&(s(t),s(i),s(o))}}}class U extends w{constructor(t){super(),g(this,t,null,_,m,{})}}export{U as component};
